01616c4587dfdd1de583dfe59c3776c864489111
KPInc425 <KPInc425@gmail.com>
2021-07-06 15:19:48 -0700

    added rps UI added media content - Rock, Paper, Scissors, and default img's updated index.html - provided the structure here rather than trying through js. added styles.css - Bulk styling added here rather than js updated rps.js to work with the added ui - rework and reorder code (Heavy WIP) included reset.css

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 Rock, paper, scissors game against the computer. Created as an interactive console game for the time being.
+
+*UI update
+The player is able to play the game by clicking on buttons rather than typing their answer in a prompt.
+
+First player to a score of 5 wins
+--Game refreshes after win/lose 
+Each button will display the player selection and an img to represent said selection
+--AI will then make a selection and display chosen img
+Evaluation, results, and End of Round statement are displayed after each selection
+Running Score always visible
+
+TODO
+Add sound effects to signify stages of play. 
+Create display to represent play (i.e. img of rock smashing scissors, etc)
--- rps.js
+++ rps.js
@@ -1,77 +1,88 @@
-// assignMoves 1, 2, 3 to ROCK, PAPER, SCISSORS
 const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
+let PLAYER_SCORE = 0;
+let AI_SCORE = 0;
+
+// // initialize ROUND_COUNTER variable to 5 for 5 rounds
+let ROUND_COUNTER = 1;
+
+let nextRound = false;
+
 let playerSelection = "";
 let playerWins;
+let ggWP = "Good Round, well played!";
+let btrLuck = "Better luck next time!";
+let playerWinsLabel = "Player Wins!";
+let playerLoseLabel = "Jarvis Wins!"
+let aiWinsLabel = "Jarvis Wins!";
+
+const mainContainer = document.querySelector('#mainContainer');
+mainContainer.style.cssText = "display: none;"
+
+const playerScoreContainer = document.querySelector('#playerScoreContainer');
+const aiScoreContainer = document.querySelector('#aiScoreContainer');
+
+const selectionsContainer = document.querySelector('#selectionsContainer');
+const evaluationContainer = document.querySelector('#evaluationContainer');
+const roundOutcomeContainer = document.querySelector('#roundOutcomeContainer');
+let roundOutcomeLabelOne = document.querySelector('#roundOutcomeLabelOne');
+let roundOutcomeLabelTwo = document.querySelector('#roundOutcomeLabelTwo');
+const endOfRoundStatementContainer = document.querySelector('#endOfRoundStatementContainer');
+const evalLabel = document.querySelector('.evalLabel');
+
+let endOfRoundStatementLabel = document.querySelector('#endOfRoundStatementLabel');
+
+
+// Start Button to populate the game layout
+const btnStartGame = document.createElement('BUTTON');
+btnStartGame.innerText = "Start Game";
+btnStartGame.addEventListener('click', () => {
+    mainContainer.style.cssText = "display: grid;"
+    rpsGame();
+})
+
+btnContainer.appendChild(btnStartGame);
 
 // gameFunction 
 function rpsGame() {
-    console.log("Game Initialized!")
-    //     initialize roundCounter variable to 5 for 5 rounds
-    let roundCounter = 1;
-//     initialize scoreCounter for player, AI to 0
-    let playerScore = 0;
-    let computerScore = 0;
-//     iterate through 5 rounds   
-    for (let i = 0; i <= 5; i++) {
-        // each round 
-        // player makes choice
-        let playerSelection = userPlay();
-        // computer makes choice
-        let computerSelection = computerPlay();
-        // FOR TESTING PURPOSES
-        // computerSelection = ROCK;
-        
-//             run rpsRound for gameplay
-        let playerWins = rpsRound(playerSelection, computerSelection);
-//             add 1 to winners score count 
-        if (playerWins == 1) {
-            playerScore += 1;
-            // Notify player of won round
-            console.log("Player takes this round!")
-            console.log("Player Score: " + playerScore)
-            console.log("Jarvis Score: " + computerScore)
-        } else if (playerWins == 2) {
-            console.log("Well a DRAW is a tie, so no one is taking this round...")
-            i -= i;
-        } else {
-            computerScore +=1;
-            console.log("Jarvis takes this round!")
-            console.log("Jarvis Score: " + computerScore)
-            console.log("Player Score: " + playerScore)
-        }
-        // if either player has reached 3 
-        if (playerScore >= 3 || computerScore >= 3) {
-            // stop the rounds
-            i = 10;
-            // declare the winner
-            if (playerScore > computerScore) {
-                console.log("Winner, Winner, Chicken Dinner!");
-                console.log("Player won with a score of " + playerScore);
-                console.log("Excellent work! You're the Best!")
-                console.log("GAME OVER");
-            } else {
-                console.log("WASTED!");
-                console.log("Jarvis won with a score of " + computerScore);
-                console.log("Don't worry, you'll get'em next time!");
-                console.log("GAME OVER");
-            }
-        }else {
-        // else 
-            //announce end of round
-            console.log("That's the end of Round #" + roundCounter);
-            // add 1 to roundCounter
-            roundCounter += 1;
-            // continue to next round
-            console.log("Get ready for Round #" + roundCounter);
-            confirm("Are you ready for the next round?");
-        }
-    }
+
+    let imgPlayerSelection = document.querySelector('#imgPlayerSelection');
+    // rpsRound functions is called
+    btnContainer.removeChild(btnStartGame);
+    const btnRock = document.createElement('BUTTON');
+    btnRock.innerText = 'ROCK!'
+    btnRock.addEventListener('click', () => {
+        imgPlayerSelection.src = "img/rock.png";
+        imgPlayerSelection.style.cssText = "display: block; margin: 0 auto 0 auto; padding: 15px;"
+        rpsRound(ROCK, computerPlay());
+    })
+
+    const btnPaper = document.createElement('BUTTON');
+    btnPaper.innerText = 'PAPER!'
+    btnPaper.addEventListener('click', () => {
+        imgPlayerSelection.src = "img/paper.png";
+        imgPlayerSelection.style.cssText = "display: block; margin: 0 auto 0 auto; padding: 15px;"
+        rpsRound(PAPER, computerPlay());
+    })
+
+    const btnScissors = document.createElement('BUTTON');
+    btnScissors.innerText = 'SCISSORS!'
+    btnScissors.addEventListener('click', () => {
+        imgPlayerSelection.src = "img/scissors.png";
+        imgPlayerSelection.style.cssText = "display: block; margin: 0 auto 0 auto; padding: 15px;"
+        rpsRound(SCISSORS, computerPlay());
+    })
+
+    btnContainer.appendChild(btnRock);
+    btnContainer.appendChild(btnPaper);
+    btnContainer.appendChild(btnScissors);
+
 }
 
 // computerPlay 
 function computerPlay() {
+    let imgAISelection = document.querySelector('#imgAISelection');
     // declare computerSelection variable
     let computerSelection = "";
     // randomChoice out of 3 
@@ -83,22 +94,24 @@ function computerPlay() {
         case (randomChoice == 1):
             //selection is rock 
             computerSelection = ROCK;
-            //console.log(computerSelection);
-            //console.log(randomChoice);
+            imgAISelection.src = "img/rock.png";
+            imgAISelection.style.cssText = "display: block; margin: 0 auto 0 auto; padding: 15px;"
             break;
         // if 2 is randomChoice 
         case (randomChoice == 2): 
             //console.log(randomChoice);
             //selection is paper
             computerSelection = PAPER;
-            //console.log(computerSelection);
+            imgAISelection.src = "img/paper.png";
+            imgAISelection.style.cssText = "display: block; margin: 0 auto 0 auto; padding: 15px;"
             break;
         // if 3 is randomChoice
         case (randomChoice == 3): 
             //console.log(randomChoice);
             //selection is scissor
             computerSelection = SCISSORS;
-            //console.log(computerSelection);
+            imgAISelection.src = "img/scissors.png";
+            imgAISelection.style.cssText = "display: block; margin: 0 auto 0 auto; padding: 15px;"
             break;
     }
     // console.log(computerSelection);
@@ -106,7 +119,7 @@ function computerPlay() {
     return computerSelection;     
 }
 
-// userPlay
+// userPlay THIS IS OBSOLETE, PLEASE REMOVE
 function userPlay() {
     // ask user for input w/ message
     // store input in variable ALL UPPERCASE
@@ -133,30 +146,111 @@ function userPlay() {
 // rpsRound 
  function rpsRound(playerSelection, computerSelection) {
     // display choices
-    showChoices(playerSelection, computerSelection);    
-    // if playerSelection AND computerSelection are EQUAL 
-    if (playerSelection === computerSelection) {
-        // output playerSelection, computerSelection, "Draw!"
-        console.log(playerSelection + " matches " + computerSelection + " that's a DRAW!" )
-        return playerWins = 2;
-    } else {
-    // else 
-        // evaluateSelections to check which wins in context 
-        //console.log("check who wins here...");
-        playerWins = evaluateSelections(playerSelection, computerSelection)
-        endScenario(playerWins, playerSelection, computerSelection);
+    showChoices(playerSelection, computerSelection);  
+    
 
-        // return gameMessage that matches current context 
-        // output playerSelection, computerSelection, gameMessage
-    } 
-    //return console.log("Round Over! Good Round!");
-    return playerWins;
+    playerWins = evaluateSelections(playerSelection, computerSelection)
+    endScenario(playerWins, playerSelection, computerSelection);
+
+    // Trigger end round
+    nextRound = true;
+
+    if (nextRound == true) {
+        if (playerWins == 1) {
+            PLAYER_SCORE += 1;
+            // Notify player of won round
+            let playerScoreLabel = document.querySelector("#playerScoreLabel");
+            playerScoreLabel.textContent = PLAYER_SCORE;
+
+            playerScoreContainer.appendChild(playerScoreLabel);
+
+            endOfRoundStatementLabel.textContent = "Player takes this round!";
+
+            console.log("Player takes this round!");
+            console.log("Player Score: " + PLAYER_SCORE);
+            console.log("Jarvis Score: " + AI_SCORE);
+            nextRound = false;
+        } else if (playerWins == 2) {
+            evalLabel.classList.add('evalLabel');
+            evalLabel.textContent = playerSelection + " matches " + computerSelection + " that's a DRAW!";
+    
+            roundOutcomeLabelOne.textContent = "Well a DRAW is a tie, so no one is taking this round...";
+            roundOutcomeLabelTwo.textContent = "";
+            endOfRoundStatementLabel.textContent = "DEAD EVEN!";
+            nextRound = false;
+        } else {
+            AI_SCORE +=1;
+
+            let aiScoreLabel = document.querySelector("#aiScoreLabel");
+            aiScoreLabel.textContent = AI_SCORE;
+
+            endOfRoundStatementLabel.textContent = "Jarvis takes this round!";
+
+            console.log("Jarvis takes this round!");
+            console.log("Jarvis Score: " + AI_SCORE);
+            console.log("Player Score: " + PLAYER_SCORE);
+            nextRound = false;
+        }
+        // if either player has reached 3 
+        if (PLAYER_SCORE >= 5 || AI_SCORE >= 5) {
+            // stop the rounds
+            // declare the winner
+            if (PLAYER_SCORE > AI_SCORE) {
+                endOfRoundStatementLabel.textContent = `Winner, Winner, Chicken Dinner! 
+                                                        Player won with a score of ${PLAYER_SCORE}
+                                                        Excellent work! You're the Best!`;
+                setTimeout(() => alert('GameOver!'), 2000);
+
+                setTimeout(() => {
+                        location.reload();
+                }, 2000)
+
+                console.log("Winner, Winner, Chicken Dinner!");
+                console.log("Player won with a score of " + PLAYER_SCORE);
+                console.log("Excellent work! You're the Best!");
+                console.log("GAME OVER");
+                nextRound = false;
+            } else {
+                endOfRoundStatementLabel.textContent = `WASTED! 
+                                                        Jarvis won with a score of ${AI_SCORE}
+                                                        Don't worry, you'll get'em next time!`;
+                setTimeout(() => alert('WASTED!'), 2000);
+
+                setTimeout(() => {
+                        location.reload();
+                }, 2000)
+
+                console.log("WASTED!");
+                console.log("Jarvis won with a score of " + AI_SCORE);
+                console.log("Don't worry, you'll get'em next time!");
+                console.log("GAME OVER");
+                nextRound = false;
+            }
+        }else {
+            //announce end of round
+            setTimeout(() => {
+                endOfRoundStatementLabel.textContent = `That's the end of Round #${ROUND_COUNTER - 1}! Get ready for Round #${ROUND_COUNTER}! `;
+            }, 2000)
+            ROUND_COUNTER += 1;
+        }
+    }
 }
 
 function showChoices(playerSelection, computerSelection) {
-    let pS = playerSelection;
-    let cS = computerSelection;
-    console.log("Player: " + pS + " || " + cS + " :Jarvis");
+    // Player selections element selected
+    let pS = document.querySelector('#playerSelection');
+    pS.textContent = playerSelection;
+
+    // Computer selection element selected
+    let cS = document.querySelector('#aiSelection');
+    cS.textContent = computerSelection;
+    
+    // VS symbol element created
+    let vs = document.querySelector('#VS')
+    vs.textContent = " VS "
+    vs.style.cssText = "font-size: 48px";
+
+    console.log("Player: " + pS.textContent + " || " + cS.textContent + " :Jarvis");
 }
 
 //evaluates selections to determine winner
@@ -164,6 +258,10 @@ function evaluateSelections(playerSelection, computerSelection) {
     // initialize selections
     let pS = playerSelection;
     let cS = computerSelection;
+    
+    if (pS == cS) {
+        return playerWins = 2;
+    }
     // evaluation of ROCK choice
     if (pS === ROCK && cS === SCISSORS) {
         // player wins
@@ -202,15 +300,45 @@ function endScenario(playerWins, playerSelection, computerSelection) {
         switch (pW == true) {
             //player won with ROCK
             case pS == ROCK:
-                console.log(pS + " SMASHES " + cS + " Player wins!");
+                // Create element for Player Rock Win
+                evalLabel.classList.add('evalLabel');
+                evalLabel.innerHTML = pS + " !SMASHES! " + cS;
+
+                roundOutcomeLabelOne.textContent = playerWinsLabel;
+
+                roundOutcomeLabelTwo.textContent = ggWP;
+
+                console.log(pS + " !SMASHES! " + cS + " Player wins!");
                 console.log("GG WP!");
                 break;  
             case pS == PAPER: 
-                console.log(pS + " COVERS " + cS + " Player wins!")
+                //player won with PAPER
+                // Create element for Player Paper Win
+                evalLabel.classList.add('evalLabel');
+                evalLabel.textContent = pS + " ^COVERS^ " + cS;
+
+                roundOutcomeLabelOne = document.querySelector('#roundOutcomeLabelOne');
+                roundOutcomeLabelOne.textContent = playerWinsLabel;
+
+                roundOutcomeLabelTwo = document.querySelector('#roundOutcomeLabelTwo');
+                roundOutcomeLabelTwo.textContent = ggWP;
+
+                console.log(pS + " ^COVERS^ " + cS + " Player wins!")
                 console.log("GG WP!")
                 break;
             case pS == SCISSORS: 
-                console.log(pS + " CUTS " + cS + " Player wins!")
+                //player won with SCISSORS
+                // Create element for Player Rock Win
+                evalLabel.classList.add('evalLabel');
+                evalLabel.textContent = pS + " /CUTS/ " + cS;
+
+                roundOutcomeLabelOne = document.querySelector('#roundOutcomeLabelOne');
+                roundOutcomeLabelOne.textContent = playerWinsLabel;
+
+                roundOutcomeLabelTwo = document.querySelector('#roundOutcomeLabelTwo');
+                roundOutcomeLabelTwo.textContent = ggWP;
+                
+                console.log(pS + " /CUTS/ " + cS + " Player wins!")
                 console.log("GG WP!")
                 break;
         }
@@ -219,14 +347,46 @@ function endScenario(playerWins, playerSelection, computerSelection) {
         switch (pW == false) {
             //player lost with ROCK
             case cS == ROCK:
+                // Create element for Jarvis Rock Win
+                evalLabel.classList.add('evalLabel');
+                evalLabel.textContent = cS + " !SMASHES! " + pS;
+
+                roundOutcomeLabelOne = document.querySelector('#roundOutcomeLabelOne');
+                roundOutcomeLabelOne.textContent = playerLoseLabel;
+
+                roundOutcomeLabelTwo = document.querySelector('#roundOutcomeLabelTwo');
+                roundOutcomeLabelTwo.textContent = btrLuck;
+
                 console.log(cS + " SMASHES " + pS + " Jarvis wins!");
                 console.log("Better Luck Next Time!");
                 break;  
             case cS == PAPER: 
+            //player lost with PAPER
+                // Create element for Jarvis Paper Win
+                evalLabel.classList.add('evalLabel');
+                evalLabel.textContent = cS + " ^COVERS^ " + pS;
+
+                roundOutcomeLabelOne = document.querySelector('#roundOutcomeLabelOne');
+                roundOutcomeLabelOne.textContent = playerLoseLabel;
+
+                roundOutcomeLabelTwo = document.querySelector('#roundOutcomeLabelTwo');
+                roundOutcomeLabelTwo.textContent = btrLuck;
+
                 console.log(cS + " COVERS " + pS + " Jarvis wins!")
                 console.log("Better Luck Next Time!")
                 break;
-            case cS == SCISSORS: 
+            case cS == SCISSORS:
+            //player lost with SCISSORS 
+                // Create element for Player Rock Win
+                evalLabel.classList.add('evalLabel');
+                evalLabel.textContent = cS + " /CUTS/ " + pS;
+
+                roundOutcomeLabelOne = document.querySelector('#roundOutcomeLabelOne');
+                roundOutcomeLabelOne.textContent = playerLoseLabel;
+
+                roundOutcomeLabelTwo = document.querySelector('#roundOutcomeLabelTwo');
+                roundOutcomeLabelTwo.textContent = btrLuck;
+
                 console.log(cS + " CUTS " + pS + " Jarvis wins!")
                 console.log("Better Luck Next Time!")
                 break;
